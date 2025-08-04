cd43525967c43bb7edbabe25bc6cd1035f1d6c1f
Blockhore <blockhore.team@gmail.com>
2025-08-04 08:28:40 +0700

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
+
+// Path
+const paths = {
+  scss: 'assets/css/style.scss',
+  dest: 'assets/css/'
+};
+
+// Compile SCSS
+gulp.task('style', function () {
+  return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(paths.dest))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.dest));
+});
+
+// Default
+gulp.task('default', gulp.series('style'));
